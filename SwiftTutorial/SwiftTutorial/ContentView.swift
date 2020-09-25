f293aaf51c6a473f23193b61add16396c2d32077
Subhra Roy <subhra.roy84@yahoo.in>
2020-09-25 21:30:44 +0530

    Text and Image

--- SwiftTutorial/SwiftTutorial/ContentView.swift
+++ SwiftTutorial/SwiftTutorial/ContentView.swift
@@ -9,8 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .center){
+            Image("costa-rica")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(20.0)
+                .padding(.all)
+                //.clipShape(Circle())
+            Text("First Line")
+                .font(.largeTitle)
+                .foregroundColor(.green)
+            Text("Second Line")
+                .font(.title)
+                .foregroundColor(.orange)
+            HStack{
+                Text("Left Side")
+                    .font(.headline)
+                    .foregroundColor(.blue)
+                Text("Right Side")
+                    .font(.headline)
+                    .foregroundColor(.pink)
+            }
+        }
     }
 }
 
