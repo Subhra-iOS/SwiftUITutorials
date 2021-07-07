cb88512913c80492d73887124bc260f459692d69
Subhra Roy <subhra.roy@e-arc.com>
2021-07-07 21:29:13 +0530

    add new binding project

---
+++ StateBindingDemo/StateBindingDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StateBindingDemo
+//
+//  Created by Subhra Roy on 07/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StateBindingDemo/StateBindingDemo/StateBindingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateBindingDemoApp.swift
+//  StateBindingDemo
+//
+//  Created by Subhra Roy on 07/07/21.
+//
+
+import SwiftUI
+
+@main
+struct StateBindingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
