2879d7d16520379251fb442939896b58fc5c18db
Subhra Roy <subhra.roy@e-arc.com>
2021-12-10 15:20:36 +0530

    Add Night Shift Proj

---
+++ NightShiftDemo/NightShiftDemo/NightShiftDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightShiftDemoApp.swift
+//  NightShiftDemo
+//
+//  Created by Subhra Roy on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct NightShiftDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
